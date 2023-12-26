933b4c4eb838630cd5f2505e88d016144054e9b5
NadeenAhmed420 <nadeenahmed420@gmail.com>
2023-12-26 11:45:45 +0200

    main

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+let sideMenu = document.querySelector("aside");
+let menuButton = document.getElementById("menu-btn");
+let closeButton = document.getElementById("close-btn");
+let toggleButton = document.querySelector(".toggle-theme");
+
+menuButton.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+
+closeButton.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
+
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-var");
+  toggleButton.querySelector("span:nth-child(1)").classList.toggle("active");
+  toggleButton.querySelector("span:nth-child(2)").classList.toggle("active");
+});
+
+orders.forEach((order) => {
+  const tr = document.createElement("tr");
+  const trContent = `
+
+  <td class="primary">${order.productName}</td>
+  <td class="text-muted">${order.productNumber}</td>
+  <td class="text-muted">${order.paymentStatus}</td>
+  <td class='${
+    order.shipping === "Declined"
+      ? "danger"
+      : order.shipping === "Pending"
+      ? "warning"
+      : "primary"
+  }'>${order.shipping}</td>
+  <td class="primary">Details</td>`;
+
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").append(tr);
+});
---
+++ scripts/order.js
@@ -0,0 +1,40 @@
+const orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "213565",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Larvender Kf1 Drone",
+    productNumber: "856213",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+
+  {
+    productName: "Ruko f11 pro Drone",
+    productNumber: "635922",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "GPS 4K  Drone",
+    productNumber: "204205",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "DJI Air pro 24",
+    productNumber: "100085",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lozengw Drone",
+    productNumber: "869242",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+];
